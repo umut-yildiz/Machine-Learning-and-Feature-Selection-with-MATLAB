9cc08a93998a451e1fab8eff242f51754cf96a53
Umut YILDIZ <78263736+umut-yildiz@users.noreply.github.com>
2022-03-05 15:13:58 +0100

    Create LowVarianceFilter.m

---
+++ Feature Selection Package/+metrics/LowVarianceFilter.m
@@ -0,0 +1,24 @@
+classdef LowVarianceFilter
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Note: For more information please look at function file.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    properties (Access = private)
+        Result
+    end
+
+    methods
+        function obj = LowVarianceFilter()
+            obj.Result = feat_sel_c.helper.MIhelper();
+        end
+        function [Rank,Score] = Calculator(obj,dataset,Result)
+            MI = obj.Result.Calculator(dataset);
+            dataset(:,MI) = []; % Clean data from infinite&missing values
+            
+            Score = var(dataset); %variance of each feature
+            [~,Rank] = sort(Score,'descend');
+            Score = sort(Score,'descend');
+
+        end
+    end
+end
