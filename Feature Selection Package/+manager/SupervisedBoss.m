c94523d11ec30551a8482d97569a04879e010aaa
Umut YILDIZ <78263736+umut-yildiz@users.noreply.github.com>
2022-04-07 16:30:22 +0200

    Update SupervisedBoss.m

--- Feature Selection Package/+manager/SupervisedBoss.m
+++ Feature Selection Package/+manager/SupervisedBoss.m
@@ -27,17 +27,12 @@ classdef SupervisedBoss
 
     methods
         function obj = SupervisedBoss()
-            obj.FS1 = feat_sel_c.decision.FS_Correlation(); % FS_Corr cleaning
-            obj.FS2 = feat_sel_c.decision.IDE_Correlation(); % IDE_Corr cleaning
             obj.FS3 = feat_sel_c.decision.LR_Correlation(); % LR_Corr cleaning
-            obj.FS4 = feat_sel_c.decision.PWFP_Correlation(); % PWFP_Corr cleaning
-            obj.FS5 = feat_sel_c.decision.Relief_Correlation(); % Relief_Corr cleaning
-            obj.FS6 = feat_sel_c.decision.LS_Correlation(); % Laplacian_Corr cleaning
 
         end
         function Manager = Calculator(obj,training)
             % Combine all methods
-            Manager = [obj.FS1.Calculator(training);obj.FS2.Calculator(training);obj.FS3.Calculator(training);obj.FS4.Calculator(training);obj.FS5.Calculator(training);obj.FS6.Calculator(training)];
+            Manager = [obj.FS3.Calculator(training)];
 
         end
     end
